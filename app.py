ec5b7e6b0bfd6e3d1f0e3226082190a815dda9e7
rheformcy <151946452+rheformcy@users.noreply.github.com>
2026-05-17 08:33:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-```python
-# =========================================================
-# IMPORT LIBRARY
-# =========================================================
 import streamlit as st
 import pandas as pd
 import numpy as np
